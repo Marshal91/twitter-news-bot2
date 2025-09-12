3b9d3a666e52c4652911e8bdde22f61138604920
Marshal91 <48273687+Marshal91@users.noreply.github.com>
2025-09-12 23:50:57 +0300

    Update news_poster.py

--- news_poster.py
+++ news_poster.py
@@ -1012,9 +1012,10 @@ if __name__ == "__main__":
     health_thread.start()
     
     # Uncomment ONE of these for testing:
-    #test_single_post("F1")
+    test_single_post("Arsenal")
     # test_simulation_mode()
     
     start_scheduler()
+
 
 
